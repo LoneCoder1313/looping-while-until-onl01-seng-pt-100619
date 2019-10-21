3e51ee365e2660d4a6ad570c4e72b3433abd2181
Ricky dildy <rdildy92@gmail.com>
2019-10-21 21:52:53 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,11 +2,13 @@ def using_while
   levitation_force = 6
 	
   while levitation_force < 10 
-  puts "Wingardium Leviosa#{levitation_force}"
-  levitation_force +=1 
-end
-#=> nil (return value)
+  it "puts out phrase'Wingardium Leviosa' while the levitation_force is incremented to 10" do 
+    looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+    expect{using_while}.to output(looping_string).to_stdout 
+   end
+   
+ end 
 
-puts "Wingardium Leviosa, Wingardium Leviosa, Wingardium Leviosa, Wingardium Leviosa, Wingardium Leviosa, Wingardium Leviosa, Wingardium Leviosa"
+
 
 
