632da2b53ad66c68cf578073d877fccdf2f94027
Ricky dildy <rdildy92@gmail.com>
2019-10-21 22:13:21 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,10 +5,8 @@ def using_while
   levitation_force +=1 
    puts 'Wingardium Leviosa' while the levitation_force is incremented to 10 do 
     looping_string = "Wingardium Leviosa" 
-    
-    
    end
-   
+   #=> nil (return value)
  end 
 
 
